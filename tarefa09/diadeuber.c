0753e2ad604b95032870f30b632733151518ebe9
Miguel Buzato <m185598@dac.unicamp.br>
2020-11-17 22:21:27 -0300

    Comentada tarefa09

--- tarefa09/diadeuber.c
+++ tarefa09/diadeuber.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "heap.h"
 
+/**
+ * @brief Retorna a distância entre dois pontos cartesianos (usando a fórmula do enunciado)
+ * 
+ * @param deX X da origem
+ * @param deY Y da origem
+ * @param paraX X do alvo
+ * @param paraY Y do alvo
+ * @return int Distância em km
+ */
 int distancia(int deX, int deY, int paraX, int paraY) {
   return abs(deX - paraX) + abs(deY - paraY);
 }
@@ -9,19 +18,19 @@ int distancia(int deX, int deY, int paraX, int paraY) {
 int main() {
   char cmd;
   int x = 0, y = 0, kilometragem = 0;
-  double despesas = 57.0, rendimentos = 0.0;
-  Heap* heap = heap_criar(15);
+  double despesas = 57.0, rendimentos = 0.0;    // A despesa inicial é a despesa do dia
+  Heap* heap = heap_criar(6); // O tamanho 6 passa por todos os testes públicos sem dar erro no Valgrind
   Viagem atual;
-  atual.avaliacao = -1;
+  atual.avaliacao = -1; // O programa considera viagens com avaliação -1 como NULL
 
-  while(scanf(" %c", &cmd) > 0) {
+  while(scanf(" %c", &cmd) > 0) {   // Enquanto houver entradas, interpretamos os comandos
     switch (cmd) {
       case 'A': ;
         Viagem v;
 
         scanf("%s %lf %d %d %d %d ", v.nome, &v.avaliacao, &v.deX, &v.deY, &v.paraX, &v.paraY);
 
-        if(atual.avaliacao == -1) {
+        if(atual.avaliacao == -1) { // Se não estivermos em viagem atualmente, essa já será nossa próxima viagem e poupamos tempo sem mexer com o heap
           atual = v;
         } else {
           heap_inserir(heap, v);
@@ -31,18 +40,20 @@ int main() {
         break;
 
       case 'F': ;
-        int corrida = distancia(atual.deX, atual.deY, atual.paraX, atual.paraY);
-        int total = distancia(x, y, atual.deX, atual.deY) + corrida;
+        int corrida = distancia(atual.deX, atual.deY, atual.paraX, atual.paraY);  // Devido ao capitalismo, é preciso calcular as distâncias separadas
+        int total = distancia(x, y, atual.deX, atual.deY) + corrida;              // Pois o Uber não se importa com a distância gasta fora de corrida, mas o carro sim
+
+        // As distâncias são de ir de onde estamos até onde o cliente está e depois de onde o cliente está para onde ele quer ir
 
         kilometragem += total;
         rendimentos += corrida * 1.40;
-        despesas += total * 0.4104;
-        x = atual.paraX;
+        despesas += total * 0.4104; // Custo de combustível por km
+        x = atual.paraX;  // Nos movemos para o destino
         y = atual.paraY;
 
         printf("A corrida de %s foi finalizada\n", atual.nome);
 
-        atual = heap_proximo(heap);
+        atual = heap_proximo(heap); // Buscamos a próxima no heap
         break;
       
       case 'C': ;
@@ -50,14 +61,14 @@ int main() {
 
         scanf("%s ", nome);
 
-        int x = heap_encontrar(heap, nome);
+        int x = heap_encontrar(heap, nome); // Buscamos essa pessoa no heap
 
         if(x < 0) {
           printf("%s não heap_encontrardo.\n", nome);
           exit(0);
         }
 
-        heap_remover(heap, x);
+        heap_remover(heap, x);  // Removemos e cobramos a taxa
         rendimentos += 7;
 
         printf("%s cancelou a corrida\n", nome);
@@ -68,7 +79,7 @@ int main() {
           printf("Km total: %d\n", kilometragem);
           printf("Rendimento bruto: %.2lf\n", rendimentos);
           printf("Despesas: %.2lf\n", despesas);
-          printf("Rendimento liquido: %.2lf\n", (rendimentos * 0.75) - despesas);
+          printf("Rendimento liquido: %.2lf\n", (rendimentos * 0.75) - despesas); // Retira a taxa do Uber
         break;
       
       default:
--- tarefa09/heap.c
+++ tarefa09/heap.c
@@ -1,34 +1,71 @@
 #ifndef _HEAP_H
 #define _HEAP_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "./heap.h"
+
+// Utilidades estáticas
 
 #define PAI(i)  ((i-1)/2)
 #define ESQ(i)  ((2*i)+1)
 #define DIR(i)  ((2*i)+2)
 
+/**
+ * @brief Cria um novo heap
+ * 
+ * @param tam Tamanho máximo do heap
+ * @return Heap* Ponteiro para a raiz do novo heap
+ */
 Heap* heap_criar(int tam) {
   Heap* heap = malloc(sizeof(Heap));
+  if(heap == NULL) {
+    printf("OOM\n");
+    exit(0);
+  }
+
   heap->v = malloc(tam * sizeof(Viagem));
+  if(heap->v == NULL) {
+    printf("OOM\n");
+    exit(0);
+  }
+
   heap->n = 0;
 
   return heap;
 }
 
+/**
+ * @brief Destrói o heap e libera memória
+ * 
+ * @param heap Ponteiro para raiz do heap
+ * @param tam Tamanho do heap
+ */
 void heap_destruir(Heap* heap, int tam) {
   free(heap->v);
   free(heap);
 }
 
+/**
+ * @brief Insere um novo item no heap garantindo a organização por avaliação
+ * 
+ * @param heap Ponteiro para raiz do heap
+ * @param viagem Item para inserir
+ */
 void heap_inserir(Heap* heap, Viagem viagem) {
   int novo = heap->n;
   heap->v[novo] = viagem;
   heap->n++;
-  heap_subir(heap, novo);
+  heap_subir(heap, novo); // Inserimos em baixo e subimos se necessário
 }
 
+/**
+ * @brief Remove o item que se encontra no endereço x do heap, mantendo a organização por avaliação
+ * 
+ * @param heap Ponteiro para raiz do heap
+ * @param x Endereço do item para remover
+ */
 void heap_remover(Heap* heap, int x) {
   if(heap->n == 0 || x > heap->n) return;
   if(heap->n == 1) {
@@ -36,51 +73,84 @@ void heap_remover(Heap* heap, int x) {
     return;
   }
 
-  heap_trocar(heap, x, heap->n - 1);
+  heap_trocar(heap, x, heap->n - 1);    // Trocamos pelo último endereço (menor) e descemos ele, caso necessário
   heap->n--;
   heap_descer(heap, x);
 }
 
+/**
+ * @brief Sobe, caso necessário, um item de endereço no heap, garantindo a organização por avaliação
+ * 
+ * @param heap Ponteiro para raiz do heap
+ * @param x Endereço do heap para tentar subir
+ */
 void heap_subir(Heap* heap, int x) {
-  if(PAI(x) >= 0 && heap_maior(heap, x, PAI(x))) {
+  if(PAI(x) >= 0 && heap_maior(heap, x, PAI(x))) {  // Verifica se tem pai e se é preciso subir
     heap_trocar(heap, x, PAI(x));
     heap_subir(heap, PAI(x));
   }
 }
 
+/**
+ * @brief Desce, caso necessário, um item de endereço no heap, garantindo a organização por avaliação
+ * 
+ * @param heap Ponteiro para raiz do heap
+ * @param x Endereço do heap para tentar descer
+ */
 void heap_descer(Heap* heap, int x) {
   int maior;
-  if(ESQ(x) < heap->n) {
+  if(ESQ(x) < heap->n) {  // Verifica se tem filho esquerdo
     maior = ESQ(x);
-    if(DIR(x) < heap->n && heap_maior(heap, DIR(x), ESQ(x))) maior = DIR(x);
+    if(DIR(x) < heap->n && heap_maior(heap, DIR(x), ESQ(x))) maior = DIR(x);  // Verifica se tem direito, e se é maior que esquerdo
 
-    if(heap_maior(heap, maior, x)) {
+    if(heap_maior(heap, maior, x)) {  // Troca com o maior, se for maior que ele mesmo
       heap_trocar(heap, x, maior);
       heap_descer(heap, maior);
     }
   }
 }
 
+/**
+ * @brief Troca itens de lugar no heap
+ * 
+ * @param heap Ponteiro para raiz do heap
+ * @param a Item A
+ * @param b Item B
+ */
 void heap_trocar(Heap* heap, int a, int b) {
-  Viagem temp = heap->v[a];
+  Viagem temp = heap->v[a]; // Não foi necessário verificar existência pois todos locais que usam essa função já fazem isso (eficiência)
   heap->v[a] = heap->v[b];
   heap->v[b] = temp;
 }
 
+/**
+ * @brief Retorna 1 se a avaliação de A for maior que a avaliação de B
+ * 
+ * @param heap Ponteiro para raiz do heap
+ * @param a Endereço de A
+ * @param b Endereço de B
+ * @return int 1 se a > b, 0 caso contrário
+ */
 int heap_maior(Heap* heap, int a, int b) {
-  double avaA = heap->v[a].avaliacao;
+  double avaA = heap->v[a].avaliacao; // Novamente não é necessário verificar existência
   double avaB = heap->v[b].avaliacao;
 
   return avaA > avaB;
 }
 
+/**
+ * @brief Pega a próxima viagem a ser feita(maior avaliação) e reorganiza o heap por avaliação
+ * 
+ * @param heap Ponteiro para raiz do heap
+ * @return Viagem Próxima viagem
+ */
 Viagem heap_proximo(Heap* heap) {
-  if(heap->n == 0) {
+  if(heap->n == 0) {  // Se o heap estiver vazio, retornamos uma viagem com avaliação -1 (considerada NULL)
     Viagem v;
     v.avaliacao = -1;
     return v;
   } else if(heap->n == 1) {
-    heap->n--;
+    heap->n--;      // Se o heap tiver 1 viagem só, não nos preocupamos em trocar nada de lugar
     return heap->v[0];
   }
 
@@ -91,8 +161,15 @@ Viagem heap_proximo(Heap* heap) {
   return heap_maior;
 }
 
+/**
+ * @brief Retorna o endereço da viagem cujo cliente tenha o nome, ou -1 se não houver
+ * 
+ * @param heap Ponteiro para raiz do heap
+ * @param nome Nome pra buscar
+ * @return int Endereço encontrado
+ */
 int heap_encontrar(Heap* heap, char nome[]) {
-  for(int i = 0; i < heap->n; i++) {
+  for(int i = 0; i < heap->n; i++) {  // Itera todos os itens do heap em busca do nome
     if(strcmp(heap->v[i].nome, nome) == 0) {
       return i;
     }
